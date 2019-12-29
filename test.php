0e321d9963a05e60dfbf884b0776d5a056aec0a5
SebastienDelrueW <58477200+SebastienDelrueW@users.noreply.github.com>
2019-12-28 20:46:23 -0800

    balises

---
+++ test.php
@@ -0,0 +1,12 @@
+<html>
+   <head>
+      <title>Titre de la page</title>
+   </head>
+   <body>
+      Texte de la page ici.
+    <?php
+    echo ('Bonjour ');
+    echo ('Bienvenue sur mon site');
+    ?>
+   </body>
+</html>
