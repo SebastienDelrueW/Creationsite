295d386a20d872c3eff80a7d9f3ec701159d3042
SebastienDelrueW <58477200+SebastienDelrueW@users.noreply.github.com>
2020-01-06 01:03:01 -0800

    paragraphe

---
+++ paragraphe.js
@@ -0,0 +1,30 @@
+/*Sélectionne le premier paragraphe du document et change son texte avec la
+ *propriété textContent que nous étudierons plus tard dans cette partie*/
+document.querySelector('p').textContent = '1er paragraphe du document';
+
+let documentDiv = document.querySelector('div'); //1er div du document
+//Sélectionne le premier paragraphe du premier div du document et modifie son texte
+documentDiv.querySelector('p').textContent = '1er paragraphe du premier div';
+
+/*Sélectionne le premier paragraphe du document avec un attribut class='bleu'
+ *et change sa couleur en bleu avec la propriété style que nous étudierons
+ *plus tard dans cette partie*/
+document.querySelector('p.bleu').style.color = 'blue';
+
+//Sélectionne tous les paragraphes du document
+let documentParas = document.querySelectorAll('p');
+
+//Sélectionne tous les paragraphes du premier div
+let divParas = documentDiv.querySelectorAll('p');
+
+/*On utilise forEach() sur notre objet NodeList documentParas pour rajouter du
+ *texte dans chaque paragraphe de notre document*/
+documentParas.forEach(function(nom, index){
+    nom.textContent += ' (paragraphe n°:' + index + ')';
+});
+
+
+
+
+
+Resources
